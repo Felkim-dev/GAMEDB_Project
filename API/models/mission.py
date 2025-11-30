ff54883a4856832f13ba60b6cbea56177fca2299
alsteve23 <pautalejandro23@gmail.com>
2025-11-30 17:20:04 -0500

    API

--- API/models/mission.py
+++ API/models/mission.py
@@ -6,8 +6,11 @@ class Mission(db.Model):
     MissionID=db.Column(db.Integer, primary_key=True)
     Title=db.Column(db.String(100), nullable=False)
     Description=db.Column(db.String(1000), nullable=False)
-    Difficulty=db.Column(db.Integer, nullable=False)
+    Difficulty=db.Column(db.Enum('Easy', 'Medium', 'Hard'), nullable=False)
 
+    #Relaciones
+    characters_link = db.relationship("CharacterMission", back_populates="mission")
+    
     #metodo para instanciar un objeto de tipo Character
     def __init__(self,Title,Description,Difficulty):
         self.Title=Title 
