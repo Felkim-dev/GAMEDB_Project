aca1c3994541a329fcbfb4d448a9ca1cad407eb6
alsteve23 <pautalejandro23@gmail.com>
2025-12-03 00:41:02 -0500

    JOINS Implementation

--- API/routes/__init__.py
+++ API/routes/__init__.py
@@ -5,6 +5,7 @@ from .mission_routes import mission_bp
 from .inventory_routes import inventory_bp
 from .char_mission_routes import char_mission_bp
 from .transaction_routes import transaction_bp
+from .report_routes import report_bp
 
 def register_routes(app):
     app.register_blueprint(player_bp, url_prefix="/players")
@@ -14,4 +15,4 @@ def register_routes(app):
     app.register_blueprint(inventory_bp, url_prefix="/inventory")
     app.register_blueprint(char_mission_bp, url_prefix="/char_missions")
     app.register_blueprint(transaction_bp, url_prefix="/transactions")
-    
+    app.register_blueprint(report_bp, url_prefix="/reports")
